41f00a0fb6bef47cb9e3761de5a23668ed0090d5
azaaaja472-byte <azaaaja472@gmail.com>
2025-10-28 11:27:05 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//1.Pilih tombol berdasarkan ID-nya
+const tombol = document.getElementaryByld('sapabutton');
+//2.Tambahkan 'event listener' untuk aksi 'click'
+tombolsapa.addEventListener('click',function() {
+  //3.jalankan kode ini ketika tombol di-klik
+   alert('halo! Terimakasih sudah berkunjung!');
+});
